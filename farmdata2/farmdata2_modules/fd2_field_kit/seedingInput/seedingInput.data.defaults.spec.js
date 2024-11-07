2257a4cb4428dc8977093b0a83619f38e407ae51
Hill134Z <zhongyu@dickinson@edu>
2024-11-07 11:47:39 -0500

    Added demo test for task 6

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -23,11 +23,23 @@ describe('Test the seeding input page', () => {
     })
 
     it('Check Data section the crop drop down does not have a selected value', () => {
+        //hillary's
 
     })
 
     it('Check Data section the crop drop down contains the correct crop list', () => {
-        
+        //hillary's
+        /* cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option4]")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option11q]") */
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]")
+            .should("have.text","ARUGULA")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option4]")
+            .should("have.text","BEAN-FAVA")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option111]")
+            .should("have.text","ZUCCHINI")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").children()
+            .should("have.length","111")
     })
 
 })
