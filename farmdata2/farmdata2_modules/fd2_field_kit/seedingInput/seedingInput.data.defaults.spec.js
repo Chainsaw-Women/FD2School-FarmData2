5d644ffcb78f5a79fa07e2e6a7b30ca16a35ccf9
kateyvk <kateyvk16@gmail.com>
2024-11-14 10:39:18 -0500

    finished test 3 and 4

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -14,11 +14,11 @@ describe('Test the seeding input page', () => {
 
     })
 
-    it('Katey-Check Data section the date input element has the default value of the current date', () => {
+    it('Check Data section the date input element has the default value of the current date', () => {
         cy.get('[data-cy=date-select').should("have.value","2024-11-14")
     })
 
-    it('Katey-Check Data section the crop drop down is enabled', () => {
+    it('Check Data section the crop drop down is enabled', () => {
         cy.get('[data-cy=dropdown-input').should("be.enabled");
         
     })
