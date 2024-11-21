39eb0ce7170845e8fa6774ea0d6581997b4645a7
Hill134Z <zhongyu@dickinson@edu>
2024-11-21 12:07:03 -0500

    Added prep work to checking if mthe filter 'all' works

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.cropFilter.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.cropFilter.spec.js
@@ -9,12 +9,43 @@ describe('Test Crop Filter', () => {
     })
     it('Check if multiple different crops are in the table when “All” is selected in the Crop filter', () => {
         //hillary
+        cy.get('[data-cy=start-date-select]')
+            .type('2019-10-01')
+        cy.get('[data-cy=end-date-select]')
+            .type('2020-01-01')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+
+        cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]')
+            .select('All')
+            .should('have.value', 'All')
         
+        cy.get('[data-cy=td-r0c1]')
+            .invoke('text')
+            .then(crop => {
+                expect(crop, 'LETTUCE-MES MIX')
+        })
+        cy.get('[data-cy=td-r2c1]')
+            .invoke('text')
+            .then(crop => {
+                expect(crop, 'SPINACH')
+        })
+        cy.get('[data-cy=td-r7c1]')
+            .invoke('text')
+            .then(crop => {
+                expect(crop, 'TURNIP')
+        })
+        // cy.get('[data-cy=r2-Crop]')
+        //     .should('have.value', 'SPINACH')
+        // cy.get('[data-cy=r7-Crop]')
+        //     .should('have.value', 'TURNIP')   
+        cy.get('[data-cy=table-body]').children()
+            .should('have.length', '8')
     })
-    it('Check the table only have seeding logs for selected crop', () => {
-        //katie
-    })
-    it('Check the dropdown for the Crop filter only has crops with seeding logs in the date range', () => {
-        //ana
-    })
+    
+    // it('Check the table only have seeding logs for selected crop', () => {
+    //     //katie
+    // })
+    // it('Check the dropdown for the Crop filter only has crops with seeding logs in the date range', () => {
+    //     //ana
+    // })
 })
